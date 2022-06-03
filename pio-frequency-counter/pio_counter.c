ed652573cc6bfff4492e157c42fff70e8d9d2ba0
Graeme Winter <graeme.winter@gmail.com>
2022-06-03 09:32:17 +0100

    WIP

--- pio-frequency-counter/pio_counter.c
+++ pio-frequency-counter/pio_counter.c
@@ -1,18 +1,60 @@
 #include <stdio.h>
 
 #include "hardware/clocks.h"
+#include "hardware/i2c.h"
 #include "hardware/pio.h"
 #include "pico/stdlib.h"
 
 #include "clock.pio.h"
 #include "counter.pio.h"
 
+// we can store at most arounf 50000 uint32_t
 #define SIZE 50000
+
+volatile uint32_t counter, counts;
+uint32_t data[SIZE];
+
+void arm();
+void disarm();
+
+// i2c setup
+#define I2C_ADDR 0x40
+
+#define GPIO_SDA0 4
+#define GPIO_SCK0 5
+
+// i2c registers - counts, delay, high, low
+uint32_t i2c_params[4];
+uint8_t *i2c_registers = (uint8_t *)i2c_params;
+uint32_t i2c_offset;
+
+void i2c0_handler() {
+  uint32_t status = i2c0_hw->intr_stat;
+  uint32_t value;
+
+  if (status & I2C_IC_INTR_STAT_R_RX_FULL_BITS) {
+    value = i2c0_hw->data_cmd;
+
+    if (value & I2C_IC_DATA_CMD_FIRST_DATA_BYTE_BITS) {
+      command = (uint8_t)(value & I2C_IC_DATA_CMD_BITS);
+      if (command == 0xff) {
+        arm();
+      } else if (command == 0x00) {
+        // set counts
+        offset = 0x0;
+      } else if (command == 0x01) {
+        // write driver settings
+        offset = 0x4;
+      }
+    } else {
+      // copy in settings
+      i2c_registers[offset++] = (uint8_t)(value & I2C_IC_DATA_CMD_BITS);
+    }
+  }
+}
 
 int main() {
   setup_default_uart();
-
-  uint32_t counts[SIZE];
 
   const uint32_t output_pin = 16;
   const uint32_t input_pin = 17;
