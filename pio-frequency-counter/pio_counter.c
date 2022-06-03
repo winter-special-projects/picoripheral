15754eacf0acc1980b50a089c942984f1cb406fd
Graeme Winter <graeme.winter@gmail.com>
2022-06-03 12:24:15 +0100

    Add frequency i2c (#2)
    * Add i2c interface
    * Divider = 5/4 -> 100 MHz by default
    * Use ns for clock; add low for driver (delay currently unused)
    * Putative Python api
    * Seems OK at 100ns / 1 kHz - but can do 100 ns at 1MHz nicely

---
+++ pio-frequency-counter/pio_counter.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+
+#include "hardware/clocks.h"
+#include "hardware/i2c.h"
+#include "hardware/irq.h"
+#include "hardware/pio.h"
+#include "pico/stdlib.h"
+
+#include "clock.pio.h"
+#include "counter.pio.h"
+
+// 50000 uint32_t is 200kB i.e. _most_ of the available RAM
+#define SIZE 50000
+
+volatile uint32_t counter, counts;
+volatile bool armed;
+uint32_t data[SIZE];
+
+// i2c setup
+#define I2C_ADDR 0x40
+
+#define GPIO_SDA0 4
+#define GPIO_SCK0 5
+
+// i2c registers - counts, delay, high, low
+volatile uint32_t i2c_params[4];
+volatile uint8_t *i2c_registers = (uint8_t *)i2c_params;
+volatile uint32_t i2c_offset;
+
+// pio pointers
+uint32_t pio_off0, pio_off1;
+
+const uint32_t output_pin = 16;
+const uint32_t input_pin = 17;
+
+void arm() {
+  // pio0 - counter
+  pio_off0 = pio_add_program(pio0, &counter_program);
+  counter_program_init(pio0, 0, pio_off0, input_pin);
+
+  // pio1 - test clock
+  pio_off1 = pio_add_program(pio1, &clock_program);
+  clock_program_init(pio1, 0, pio_off1, output_pin);
+
+  // clock low into pio; move to isr; push high to pio
+  pio1->txf[0] = (i2c_params[3] / 10) - 3;
+  pio_sm_exec(pio1, 0, pio_encode_pull(false, false));
+  pio_sm_exec(pio1, 0, pio_encode_out(pio_isr, 32));
+  pio1->txf[0] = (i2c_params[2] / 10) - 3;
+
+  printf("Arming with %d / %d\n", i2c_params[2], i2c_params[3]);
+
+  pio_sm_set_enabled(pio1, 0, true);
+  pio_sm_set_enabled(pio0, 0, true);
+  armed = true;
+}
+
+void disarm() {
+  // disable
+  pio_sm_set_enabled(pio0, 0, false);
+  pio_sm_set_enabled(pio1, 0, false);
+
+  // remove programs to reset PIO
+  pio_remove_program(pio0, &counter_program, pio_off0);
+  pio_remove_program(pio1, &clock_program, pio_off1);
+}
+
+void i2c0_handler() {
+  uint32_t status = i2c0_hw->intr_stat;
+  uint32_t value;
+
+  uint8_t command;
+
+  if (status & I2C_IC_INTR_STAT_R_RX_FULL_BITS) {
+    value = i2c0_hw->data_cmd;
+
+    if (value & I2C_IC_DATA_CMD_FIRST_DATA_BYTE_BITS) {
+      command = (uint8_t)(value & I2C_IC_DATA_CMD_BITS);
+      if (command == 0xff) {
+        arm();
+      } else if (command == 0x00) {
+        // set counts
+        i2c_offset = 0x0;
+      } else if (command == 0x01) {
+        // write clock settings
+        i2c_offset = 0x4;
+      }
+    } else {
+      // copy in settings
+      i2c_registers[i2c_offset++] = (uint8_t)(value & I2C_IC_DATA_CMD_BITS);
+    }
+  }
+}
+
+int main() {
+  setup_default_uart();
+
+  printf("Startup\n");
+
+  // i2c
+  i2c_init(i2c0, 100e3);
+  i2c_set_slave_mode(i2c0, true, I2C_ADDR);
+
+  gpio_set_function(GPIO_SDA0, GPIO_FUNC_I2C);
+  gpio_set_function(GPIO_SCK0, GPIO_FUNC_I2C);
+  gpio_pull_up(GPIO_SDA0);
+  gpio_pull_up(GPIO_SCK0);
+
+  i2c0_hw->intr_mask =
+      I2C_IC_INTR_MASK_M_RD_REQ_BITS | I2C_IC_INTR_MASK_M_RX_FULL_BITS;
+
+  irq_set_exclusive_handler(I2C0_IRQ, i2c0_handler);
+  irq_set_enabled(I2C0_IRQ, true);
+
+  printf("i2c enabled at %d\n", I2C_ADDR);
+
+  armed = false;
+
+  // sensible defaults...
+  i2c_params[1] = 0;
+  i2c_params[2] = 50000;
+  i2c_params[3] = 50000;
+
+  while (true) {
+    // wait until we are ready to go
+    while (!armed) {
+      tight_loop_contents();
+    }
+
+    for (int j = 0; j < i2c_params[0]; j++) {
+      data[j] = pio_sm_get_blocking(pio0, 0);
+    }
+
+    disarm();
+
+    for (int j = 0; j < i2c_params[0]; j++) {
+      uint32_t ticks = data[j] - 1;
+      if (ticks & 0x80000000) {
+        ticks = 5 * (0xffffffff - ticks);
+        printf("High: %d %d\n", ticks * 10, j);
+      } else {
+        ticks = 5 * (0x7fffffff - ticks);
+        printf("Low:  %d %d\n", ticks * 10, j);
+      }
+    }
+  }
+}
---
+++ pio-frequency-counter/pio_counter.py
@@ -0,0 +1,25 @@
+import smbus
+import struct
+import time
+
+
+class PioCounter:
+    def __init__(self):
+        # connectivity
+        self._i2c_address = 0x40
+        self._smbus = smbus.SMBus(1)
+
+    def setup(self, count, delay, high, low):
+        message = list(struct.pack("I", count))
+        self._smbus.write_i2c_block_data(self._i2c_address, 0x00, message)
+        message = list(struct.pack("III", delay, high, low))
+        self._smbus.write_i2c_block_data(self._i2c_address, 0x01, message)
+
+    def arm(self):
+        self._smbus.write_i2c_block_data(self._i2c_address, 0xFF, [])
+
+
+if __name__ == "__main__":
+    pc = PioCounter()
+    pc.setup(500, 0, 100, 999900)
+    pc.arm()
