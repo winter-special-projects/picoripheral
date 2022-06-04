8300c92a5e81757ccd28d0ac3945c6b0e4819d8e
Graeme Winter <graeme.winter@gmail.com>
2022-06-04 05:46:56 +0100

    Barely functional fetcher

--- pio-frequency-counter/pio_counter.py
+++ pio-frequency-counter/pio_counter.py
@@ -1,6 +1,8 @@
-import smbus
 import struct
 import time
+
+import smbus
+import spidev
 
 
 class PioCounter:
@@ -8,8 +10,10 @@ class PioCounter:
         # connectivity
         self._i2c_address = 0x40
         self._smbus = smbus.SMBus(1)
+        self._count = 0
 
     def setup(self, count, delay, high, low):
+        self._count = count
         message = list(struct.pack("I", count))
         self._smbus.write_i2c_block_data(self._i2c_address, 0x00, message)
         message = list(struct.pack("III", delay, high, low))
@@ -18,8 +22,24 @@ class PioCounter:
     def arm(self):
         self._smbus.write_i2c_block_data(self._i2c_address, 0xFF, [])
 
+    def read(self):
+        spi = spidev.SpiDev()
+        spi.open(0, 0)
+        spi.mode = 3
+        spi.bits_per_word = 8
+        spi.max_speed_hz = 10_000_000
+
+        zero = [0 for j in range(4 * self._count)]
+        data = bytearray(spi.xfer3(zero))
+        spi.close()
+
+        return struct.unpack(f"{self._count}I", data)
+
 
 if __name__ == "__main__":
     pc = PioCounter()
-    pc.setup(500, 0, 100, 999900)
+    pc.setup(500, 0, 100, 999_900)
     pc.arm()
+    data = pc.read()
+    for j, d in enumerate(data):
+        print(j, d)
